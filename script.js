6d3e283f58b2437bb9c49713e0f829ecb3f014d4
marbahi <ammar.skyagt@gmail.com>
2025-12-09 17:56:57 +0700

    Adding operate function
    I add operate function that contains four basic math function
    and operate function tajes first number, last number, and operator

--- script.js
+++ script.js
@@ -15,5 +15,17 @@ function divide(fNum, LNum) {
 }
 
 let firstNumber;
-let lastNUmber;
+let lastNumber;
 let operator;
+
+function operate(firstNumber, lastNumber, operator) {
+    if(operator === "+") {
+        adding(firstNumber, lastNumber);
+    } else if(operator === "-") {
+        subtract(firstNumber, lastNumber);
+    } else if(operator === "*") {
+        multiply(firstNumber, lastNumber);
+    } else if(operator === "/") {
+        divide(firstNumber, lastNumber);
+    }
+}
